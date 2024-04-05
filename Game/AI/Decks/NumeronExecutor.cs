0941fda6c37a645262b10454f8d0a4c8a8cfb244
Waseef Nayeem <nayeemw@mcmaster.ca>
2024-04-05 03:25:54 -0400

    Fixed Numeron network activation, added more executors and predicates
    Replaced droplet with chalice

--- Game/AI/Decks/NumeronExecutor.cs
+++ Game/AI/Decks/NumeronExecutor.cs
@@ -1,15 +1,13 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices.WindowsRuntime;
+using YGOSharp.OCGWrapper.Enums;
 
 namespace WindBot.Game.AI.Decks
 {
     [Deck("Numeron", "AI_Numeron")]
     public class NumeronExecutor : DefaultExecutor
     {
-
         public class CardId
         {
             // Effect Monsters
@@ -17,7 +15,7 @@ namespace WindBot.Game.AI.Decks
             public const int Gameciel = 55063751;
             public const int PlanetPathfinder = 97526666;
             public const int AshBlossom = 14558127;
-            //public const int GhostMourner = 52038441;
+            // public const int GhostMourner = 52038441;
             public const int GhostBelle = 73642296;
             public const int NumeronWall = 42352091;
             public const int EffectVeiler = 97268402;
@@ -30,7 +28,8 @@ namespace WindBot.Game.AI.Decks
             public const int NumeronCalling = 77402960;
             public const int CosmicCyclone = 08267140;
             public const int LimiterRemoval = 23171610;
-            public const int ForbiddenDroplet = 24299458;
+            //public const int ForbiddenDroplet = 24299458;
+            public const int ForbiddenChalice = 25789292;
             public const int NumeronNetwork = 41418852;
 
             // Imperm lol
@@ -75,7 +74,6 @@ namespace WindBot.Game.AI.Decks
         };
 
         private bool summonedPathfinder = false;
-        private bool sentCallingToGY = false;
         private bool usedCallingEffect = false;
 
         public NumeronExecutor(GameAI ai, Duel duel) : base(ai, duel)
@@ -84,12 +82,17 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Summon, CardId.PlanetPathfinder, PlanetPathfinderSummon);
             AddExecutor(ExecutorType.SpSummon, CardId.LavaGolem, LavaGolemSummon);
             AddExecutor(ExecutorType.SpSummon, CardId.Gameciel, GamecielSummon);
+            AddExecutor(ExecutorType.SpSummon, CardId.NumeronWall, NumeronWallSummon);
+
+            AddExecutor(ExecutorType.Activate, CardId.Raigeki, DefaultRaigeki);
+            AddExecutor(ExecutorType.Activate, CardId.LightningStorm, LightningStormActivate);
 
             // Field Searchers
             AddExecutor(ExecutorType.Activate, CardId.PlanetPathfinder, NeedToSearchField);
-            AddExecutor(ExecutorType.Activate, CardId.NumeronWall, NeedToSearchField);
+            AddExecutor(ExecutorType.Activate, CardId.NumeronWall, NumeronWallActivate);
 
-            AddExecutor(ExecutorType.Activate, CardId.NumeronNetwork, NumeronNetworkActivate);
+            AddExecutor(ExecutorType.Activate, CardId.NumeronNetwork, ActivateNetworkFromHand);
+            AddExecutor(ExecutorType.Activate, CardId.NumeronNetwork, ActivateNetworkOnField);
 
             // Hand Traps
             AddExecutor(ExecutorType.Activate, CardId.AshBlossom, DefaultAshBlossomAndJoyousSpring);
@@ -99,13 +102,25 @@ namespace WindBot.Game.AI.Decks
             AddExecutor(ExecutorType.Activate, CardId.Impermanence, DefaultInfiniteImpermanence);
 
             AddExecutor(ExecutorType.Activate, CardId.CosmicCyclone, DefaultCosmicCyclone);
-            
+
+            AddExecutor(ExecutorType.Activate, CardId.LimiterRemoval, LimiterRemovalActivate);
+
+            AddExecutor(ExecutorType.SpellSet, CardId.Impermanence, SetSpellOrTrap);
+            AddExecutor(ExecutorType.SpellSet, CardId.ForbiddenChalice, SetSpellOrTrap);
+
         }
 
         public override bool OnSelectHand()
         {
             // Always go second
             return false;
+        }
+
+        public override void OnNewTurn()
+        {
+            // TODO: Figure out new turn logic later
+            summonedPathfinder = false;
+            usedCallingEffect = false;
         }
 
         private bool LavaGolemSummon()
@@ -126,13 +141,10 @@ namespace WindBot.Game.AI.Decks
 
         private bool PlanetPathfinderSummon()
         {
+            if (Bot.HasInSpellZone(CardId.NumeronNetwork)) return false;
+
             if (Bot.HasInHand(CardId.LavaGolem)) {
-                if (Bot.HasInHand(CardId.NumeronWall)
-                || Bot.HasInHand(CardId.Terraforming)
-                || Bot.HasInHand(CardId.NumeronNetwork))
-                {
-                    return false;
-                }
+                if (Bot.HasInHand(CardId.NumeronWall) || Bot.HasInHand(CardId.Terraforming) || Bot.HasInHand(CardId.NumeronNetwork)) return false;
             }
 
             summonedPathfinder = true;
@@ -141,26 +153,87 @@ namespace WindBot.Game.AI.Decks
 
         private bool NeedToSearchField()
         {
-            return Bot.IsFieldEmpty() && !Bot.HasInHand(CardId.NumeronNetwork);
+            return !Bot.HasInSpellZone(CardId.NumeronNetwork) && !Bot.HasInHand(CardId.NumeronNetwork);
         }
 
-        private bool NumeronNetworkActivate()
+        private bool ActivateNetworkFromHand()
         {
             // Play from hand
-            if (Bot.IsFieldEmpty() && Bot.HasInHand(CardId.NumeronNetwork)) return true;
+            if (!Bot.HasInSpellZone(CardId.NumeronNetwork) && Card.Location == CardLocation.Hand) return true;
 
+            return false;
+        }
+
+        private bool ActivateNetworkOnField()
+        {
             // Using Network's base effect
-            if (!sentCallingToGY)
+            if (Card.Location != CardLocation.SpellZone) return false;
+
+            if (!Bot.HasInGraveyard(CardId.NumeronCalling))
             {
                 AI.SelectCard(CardId.NumeronCalling);
-                sentCallingToGY = true;
+                AI.SelectNextCard(CardId.N1_Ekam);
+                AI.SelectNextCard(CardId.N2_Dve);
+                AI.SelectNextCard(CardId.N3_Trini);
+                AI.SelectNextCard(CardId.N4_Catvari);
                 return true;
             }
 
-            // Using Calling's inherited effect
-            if (sentCallingToGY && !usedCallingEffect) {
-                AI.SelectCard(CardId.NC1_Sunya, CardId.N2_Dve, CardId.N3_Trini, CardId.N4_Catvari);
-                usedCallingEffect = true;
+            return false;
+        }
+
+        private bool SetSpellOrTrap()
+        {
+            return Duel.Turn == 1 || Duel.Phase == DuelPhase.Main2;
+        }
+
+        private bool LimiterRemovalActivate()
+        {
+            return Duel.Phase == DuelPhase.Damage && Bot.BattlingMonster.HasRace(CardRace.Machine);
+        }
+
+        private bool LightningStormActivate()
+        {
+            if (Bot.HasInHand(CardId.Raigeki))
+            {
+                AI.SelectPlace(Zones.SpellZones);
+                return true;
+            }
+
+            if (Enemy.HasAttackingMonster())
+            {
+                AI.SelectPlace(Zones.MonsterZones);
+                return true;
+            }
+
+            if (!Enemy.HasAttackingMonster() && Enemy.GetSpellCount() > 0)
+            {
+                if (Bot.HasInHand(CardId.FeatherDuster))
+                {
+                    return false;
+                }
+
+                AI.SelectPlace(Zones.SpellZones);
+                return true;
+            } 
+
+            return false;
+        }
+
+        private bool NumeronWallSummon()
+        {
+            return Duel.Player == 1 && Bot.UnderAttack;
+        }
+
+        private bool NumeronWallActivate()
+        {
+            if (Duel.LastChainPlayer == 1 && Duel.CurrentChain.Contains(Card))
+            {
+                return true;
+            }
+
+            if (Duel.Player == 0 && !Bot.HasInHand(CardId.NumeronNetwork))
+            {
                 return true;
             }
 
